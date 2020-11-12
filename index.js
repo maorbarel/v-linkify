44dbc3cca3be2cb9a333a3bf886b03c6e149a64c
maor barel <maorrbarel@gmail.com>
2020-11-13 01:10:34 +0200

    let to var

--- index.js
+++ index.js
@@ -1,26 +1,26 @@
 export const vLinkify = {
    bind: function (el, binding) {
-      let urlPattern = /\b(?:https?|ftp):\/\/[a-z0-9-+&@#\/%?=~_|!:,.;]*[a-z0-9-+&@#\/%=~_|]/gim;
-      let pseudoUrlPattern = /(^|[^\/])(www\.[\S]+(\b|$))/gim;
-      let emailAddressPattern = /[\w.]+@[a-zA-Z_-]+?(?:\.[a-zA-Z]{2,6})+/gim;
+      var urlPattern = /\b(?:https?|ftp):\/\/[a-z0-9-+&@#\/%?=~_|!:,.;]*[a-z0-9-+&@#\/%=~_|]/gim;
+      var pseudoUrlPattern = /(^|[^\/])(www\.[\S]+(\b|$))/gim;
+      var emailAddressPattern = /[\w.]+@[a-zA-Z_-]+?(?:\.[a-zA-Z]{2,6})+/gim;
 
-      let modifiedText = el.textContent;
+      var modifiedText = el.textContent;
 
-      let matchedResults = [
+      var matchedResults = [
          ...modifiedText.matchAll(emailAddressPattern), 
          ...modifiedText.matchAll(urlPattern), 
          ...modifiedText.matchAll(pseudoUrlPattern)
       ].flat();
 
       if (matchedResults) {
-         let options = createOptions(binding?.value)
+         var options = createOptions(binding?.value)
 
          matchedResults.forEach(result => {
             if (result.match(emailAddressPattern)?.length) {
                modifiedText = modifiedText.replace(result, `<a ${options} href="mailto:${result}">${result}</a>`)
             }
             if (result.match(urlPattern)?.length || result.match(pseudoUrlPattern)?.length) {
-               let prefix = result.toLowerCase().indexOf('http') === -1 && result.toLowerCase().indexOf('ftp') === -1 ? '//' : '';
+               var prefix = result.toLowerCase().indexOf('http') === -1 && result.toLowerCase().indexOf('ftp') === -1 ? '//' : '';
                modifiedText = modifiedText.replace(result, `<a ${options} href="${prefix + result.trim()}">${result}</a>`)
             }
          });
@@ -35,8 +35,8 @@ export default {
 }
 function createOptions(params){
    if(typeof params === 'object' && params !== null && !Array.isArray(params)){
-      let className = params?.className ? `class="${params.className}"` : '';
-      let target = `target="${params.target || '_blank'}"`;
+      var className = params?.className ? `class="${params.className}"` : '';
+      var target = `target="${params.target || '_blank'}"`;
       return `${className} ${target}`
    }else{
       return `target="_blank"`
